efb14cf348ed1205cf6c48187cd62d47e02c3f04
alemastro97 <alessandro.mastropasqua@mail.polimi.it>
2023-10-14 22:19:11 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='check-filetype',
+    version='1.0.0',
+    packages=['check_filetype'],
+    install_requires=[
+        # List your package's dependencies here
+    ],
+)
