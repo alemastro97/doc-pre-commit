3a88fd4f280e6c8aa3175b1ebdef7e8c95fc7777
alemastro97 <alessandro.mastropasqua@mail.polimi.it>
2023-10-14 22:24:39 +0200

    hange setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='check-filetype',
     version='1.0.0',
-    packages=['check_filetype'],
+    py_modules=['check_filetype'],
     install_requires=[
         # List your package's dependencies here
     ],
