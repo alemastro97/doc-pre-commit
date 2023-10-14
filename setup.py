78d677f3883f3abe24b53a9c644c08782d825cf7
alemastro97 <alessandro.mastropasqua@mail.polimi.it>
2023-10-14 22:29:13 +0200

    Test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='check-filetype',
-    version='1.0.0',
+    version='0.1.0',
     py_modules=['check_filetype'],
     install_requires=[
         # List your package's dependencies here
